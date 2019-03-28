54d0adf95c4f3ca9a9f3c85a19882124275ce997
Micah Fischer <42282649+MFischer981@users.noreply.github.com>
2019-03-27 19:38:49 -0700

    Added comments to js file.
    Added comments to js file. Added name and date to file heading.

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+// Micah Fischer 3/27/19
+
+// Function deletes one character from the end of the string.
 function backspace() {
     var textbox = document.getElementById('displaybox');
     if (textbox.value) {
@@ -5,14 +8,18 @@ function backspace() {
     }
 }
 
+// Set the default decimal length to four.
 var decimal = 4;
+// Empty array for calculator history.
 var x = [];
 var mathHistory = 0;
 
+// Set the decimal length to the value of the input box.
 function setDecimal() {
     decimal = document.getElementById('decimalbox').value;
 }
 
+// Evaluate the input and save the formula to the history string.
 function solve() {
     x.unshift(document.getElementById('displaybox').value);
     console.log(x)
@@ -20,6 +27,7 @@ function solve() {
     mathHistory = 0;
 }
 
+// Access the previous equation in the history string.
 function back() {
     if (x.length > mathHistory) {
         document.getElementById('displaybox').value = x[mathHistory];
